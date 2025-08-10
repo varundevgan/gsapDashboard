153a9293b6eeb8489b16781d14ca4c8fc6de96e4
Varun Devgan <varun.devgan@outlook.com>
2025-08-10 11:49:19 +0530

    Signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Signup from './Auth/Signup'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Dashboard from './components/dashboard/Dashboard'
 
 const App = () => {
   return (
    <Router>
       <Routes>
         <Route>
-          <Route key={'login-in'} exact path='/login-in' element={<Signup />} />
+          <Route key={'login-in'} exact path='/login' element={<Signup />} />
           <Route key={'sign-in'} exact path='/sign-up' element={<Signup />} />
+          <Route path='dashboard' element={<Dashboard />} />
         </Route>
       </Routes>
    </Router>
--- src/Auth/Signup.jsx
+++ src/Auth/Signup.jsx
@@ -2,25 +2,37 @@ import React, { useEffect, useState } from 'react'
 import { FaUser } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 import { gsap } from 'gsap';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
 
-    const [showSignup, setShowSignup] = useState(false)
+    const [showSignup, setShowSignup] = useState(true)
     const navigate = useNavigate()
-    const location = useLocation()
+    const [width, setWidth] = useState()
 
     const matchMedia = gsap.matchMedia()
 
     function login(e){
         e.preventDefault()
         console.log('clicked')
+        navigate('/dashboard')
+    }
+
+    function signUp(e){
+        e.preventDefault()
+        console.log('clicked')
+        setShowSignup(!showSignup)
+        navigate('/login')
     }
 
     function showSignupPage(e){
         e.preventDefault()
         setShowSignup(!showSignup)
         
+    }
+
+    window.addEventListener('resize',()=>setWidth(window.innerWidth))
+    useEffect(()=>{
         matchMedia.add('(min-width: 1024px)', () => {
             if(!showSignup){
                 navigate('/sign-up')
@@ -41,7 +53,7 @@ const Signup = () => {
                 })
             }
             else{
-                navigate('/login-in')
+                navigate('/login')
     
                 gsap.to('#bigBalls',{
                 x:'0%',
@@ -59,6 +71,17 @@ const Signup = () => {
                 })
             }
         })
+
+        if(width < 1024){
+            gsap.to('#bigBalls, #login, #signup',{
+                x:0,
+            })
+        }
+        if(width > 1024){
+            gsap.to('#bigBalls, #login, #signup',{
+                y:0,
+            })
+        }
 
         matchMedia.add('(max-width: 1023px)',() => {
              if(!showSignup){
@@ -80,10 +103,10 @@ const Signup = () => {
                 })
             }
             else{
-                navigate('/login-in')
+                navigate('/login')
     
                 gsap.to('#bigBalls',{
-                y:'0%',
+                y:'-10%',
                 duration: 1
                 })
                 gsap.to('#signup',{
@@ -98,22 +121,31 @@ const Signup = () => {
                 })
             }
         })
-        
-    }
+
+        setTimeout(()=>{
+            return () => {
+                matchMedia.revert()
+                window.removeEventListener('resize',()=>setWidth(window.innerWidth))
+            }
+        },3000)
+
+    },[showSignup,width])
+
+
     
 
 
   return (
-    <div className='w-full relative flex flex-col lg:flex-row  justify-between h-screen bg-gray-900 bg-[url(https://i.postimg.cc/RFqSM2rc/bg.jpg)] bg-no-repeat bg-cover bg-center overflow-hidden '>
+    <div className='w-full relative flex flex-col lg:flex-row  justify-between h-screen bg-gray-900 bg-[url(https://i.postimg.cc/RFqSM2rc/bg.jpg)] bg-no-repeat bg-cover bg-center overflow-hidden'>
         {/* signup */}
-            <div id='signup' className={`flex w-full justify-center opacity-0 `}>
-            <div className='absolute  lg:left-[10%] lg:bottom-[10%]  border-1 border-[rgba(255,255,255,0.2)] rounded-[20px] backdrop-blur-xl px-12 py-10'>
-           <form onSubmit={login} className='flex h-120 flex-col justify-between'>
-                <div>
+            <div id='signup' className={`flex w-full justify-center opacity-0 z-[10] `}>
+            <div className='absolute  lg:left-[10%] lg:bottom-[10%]  border-1 border-[rgba(255,255,255,0.2)] rounded-[20px] backdrop-blur-xl px-6 py-4 sm:px-12 sm:py-10 w-70 sm:w-auto'>
+           <form onSubmit={signUp} className='flex h-110 sm:h-120 flex-col sm:justify-between'>
+                <div className='mb-4'>
                     <h1 className='text-bold uppercase tracking-wide text-4xl text-white text-center font-bold'>Sign Up</h1>
                 </div>
                 <div className='flex flex-col gap-7'>
-                    <label className='w-75 px-5 py-3.5 flex justify-between items-center border-1 border-[rgba(255,255,255,0.2)]  rounded-[30px]'>
+                    <label className='w-60 sm:w-75 px-5 py-3.5 flex justify-between items-center border-1 border-[rgba(255,255,255,0.2)]  rounded-[30px]'>
                         <input 
                             type="text"
                             placeholder='Enter Username'
@@ -121,7 +153,7 @@ const Signup = () => {
                             />
                         <FaUser className='text-white text-sm' />
                     </label>
-                    <label className='w-75 px-5 py-3.5 flex justify-between items-center border-1 border-[rgba(255,255,255,0.2)]  rounded-[30px]'>
+                    <label className='w-60 sm:w-75 px-5 py-3.5 flex justify-between items-center border-1 border-[rgba(255,255,255,0.2)]  rounded-[30px]'>
                         <input 
                             type="text"
                             placeholder='Enter Email'
@@ -129,7 +161,7 @@ const Signup = () => {
                             />
                         <FaUser className='text-white text-sm' />
                     </label>
-                    <label className='w-full px-5 py-3.5 flex justify-between items-center border-1 border-[rgba(255,255,255,0.2)] rounded-[30px]'>
+                    <label className='w-60 sm:w-75 mb-10 sm:mb-0 px-5 py-3.5 flex justify-between items-center border-1 border-[rgba(255,255,255,0.2)] rounded-[30px]'>
                         <input 
                             type="password" 
                             placeholder='Enter your password'
@@ -139,7 +171,7 @@ const Signup = () => {
                         <FaEyeSlash className='text-white' />
                     </label>
                 </div>
-                <div className='w-full flex items-center justify-between'>
+                <div className='w-full flex flex-col sm:flex-row gap-2 items-center justify-between mt-[-20px] mb-2'>
                     <div className='flex gap-1 items-center text-white tracking-wide font-[400]'>
                         <input 
                             type="checkbox" 
@@ -148,29 +180,29 @@ const Signup = () => {
                         <span className='text-[16px]'>Remember Me</span>
                     </div>
                     <div>
-                        <span><a onClick={showSignupPage} className='text-white decoration-none text-[16px]' href="#">Forgot Password?</a></span>
+                        <span><a onClick={showSignupPage} className='text-white decoration-none text-[16px]' href="#">Login Account !</a></span>
                     </div>
                 </div>
                 <div>
-                    <button className='w-full bg-white text-black py-2.5 px-5 rounded-[50px] font-bold' type='submit'>Sign Up</button>
+                    <button className='w-full bg-white text-black py-2.5 px-5 rounded-[50px] font-bold mt-2 sm:mt-0 cursor-pointer' type='submit'>Sign Up</button>
                 </div>
                 <div></div>
            </form>
         </div>
             </div>
         {/* big balls */}
-        <div id='bigBalls' className='w-full h-[90%] top-[-60%] md:top-[-50%] lg:w-[180%]  lg:h-[200%] absolute lg:top-[-100%] lg:left-[-125%]  bg-white rounded-full'>
+        <div id='bigBalls' className='w-full h-[90%] top-[-60%] md:top-[-50%] lg:w-[180%] lg:h-[180%] xl:h-[200%] absolute lg:top-[-100%] lg:left-[-125%]  bg-white rounded-full z-[1]'>
 
         </div>
         {/* login */}
-            <div id='login' className={`flex w-full justify-center`}>
-            <div className='absolute mx-auto bottom-[5%] lg:right-[10%] lg:bottom-[10%]  border-1 border-[rgba(255,255,255,0.2)] rounded-[20px] backdrop-blur-xl px-12 py-10'>
-           <form onSubmit={login} className='flex h-100 flex-col justify-between'>
+            <div id='login' className={`flex w-full justify-center z-[10]`}>
+            <div className='absolute mx-auto bottom-[5%] lg:right-[10%] lg:bottom-[10%]  border-1 border-[rgba(255,255,255,0.2)] rounded-[20px] backdrop-blur-xl px-6 py-4 sm:px-12 sm:py-10 w-70 sm:w-auto'>
+           <form onSubmit={login} className='flex h-110 sm:h-120 flex-col justify-between'>
                 <div>
                     <h1 className='text-bold uppercase tracking-wide text-4xl text-white text-center font-bold'>Login</h1>
                 </div>
                 <div className='flex flex-col gap-7'>
-                    <label className='w-75 px-5 py-3.5 flex justify-between items-center border-1 border-[rgba(255,255,255,0.2)]  rounded-[30px]'>
+                    <label className='w-60 sm:w-75 px-5 py-3.5 flex justify-between items-center border-1 border-[rgba(255,255,255,0.2)]  rounded-[30px]'>
                         <input 
                             type="text"
                             placeholder='Enter Username'
@@ -178,7 +210,7 @@ const Signup = () => {
                             />
                         <FaUser className='text-white text-sm' />
                     </label>
-                    <label className='w-full px-5 py-3.5 flex justify-between items-center border-1 border-[rgba(255,255,255,0.2)] rounded-[30px]'>
+                    <label className='w-60 sm:w-75 px-5 py-3.5 flex justify-between items-center border-1 border-[rgba(255,255,255,0.2)] rounded-[30px]'>
                         <input 
                             type="password" 
                             placeholder='Enter your password'
@@ -188,7 +220,7 @@ const Signup = () => {
                         <FaEyeSlash className='text-white' />
                     </label>
                 </div>
-                <div className='w-full flex items-center justify-between'>
+                <div className='w-full flex flex-col sm:flex-row items-center justify-between gap-2'>
                     <div className='flex gap-1 items-center text-white tracking-wide font-[400]'>
                         <input 
                             type="checkbox" 
@@ -197,11 +229,11 @@ const Signup = () => {
                         <span className='text-[16px]'>Remember Me</span>
                     </div>
                     <div>
-                        <span><a  onClick={showSignupPage} className='text-white decoration-none text-[16px]' href="#">Forgot Password?</a></span>
+                        <span><a  onClick={showSignupPage} className='text-white decoration-none text-[16px]' href="#">Create Account?</a></span>
                     </div>
                 </div>
                 <div>
-                    <button className='w-full bg-white text-black py-2.5 px-5 rounded-[50px] font-bold' type='submit'>Login</button>
+                    <button className='w-full bg-white text-black py-2.5 px-5 rounded-[50px] font-bold cursor-pointer' type='submit'>Login</button>
                 </div>
                 <div></div>
            </form>
